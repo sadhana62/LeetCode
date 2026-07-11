9fb3adfca0c2f0f0bd29f8f1da3ac07eca1887a7
Sadhana Shrivastava <71784646+sadhana62@users.noreply.github.com>
2026-07-11 14:49:45 +0530

    Time: 23 ms (54.67%), Space: 17.5 MB (23.8%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,40 +1,38 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        vector<vector<int>> res;
-        sort(nums.begin(), nums.end());
-        for (int j = 0; j < nums.size(); j++) {
-             if (j > 0 && nums[j] == nums[j - 1]) continue;
-            for (int i = j+1; i < nums.size(); i++) {
-               
-                if (i > j+1 && nums[i] == nums[i - 1])
-                    continue;
+        int n = nums.size();
+        vector<vector<int>> ans;
+        sort(nums.begin(),nums.end());
+        for (int i = 0;i<n;i++) {
+            if (i > 0 && nums[i] == nums[i-1]) continue;
+            for (int j = i+1;j<n;j++) {
+                 if (j > i+1 && nums[j] ==  nums[j-1]) continue;
+                 int k = j+1;
+                 int s = n-1;
+                 while (k < s) {
+                 long long sum = (long long) nums[i] + nums[j] + nums[k] + nums[s];
+                 if (sum == target)  {
+                     vector<int> temp {nums[i],nums[k],nums[s],nums[j] };
+                     ans.push_back(temp);
+                     k++;
+                     s--;
+                     while (k<s && nums[k] == nums[k-1]) {
+                        k++;
+                     }
+                      while (k<s && nums[s] == nums[s+1]) {
+                        s--;
+                     }
+                 } else if (sum > target) {
+                    s--;
+                 } else {
+                    k++;
+                 }
 
-                int left = i + 1;
-                int right = nums.size() - 1;
-
-                while (left < right) {
-                    long long sum = (long long) nums[j] + nums[i] + nums[left] + nums[right];
-                    if (sum == target) {
-                        vector<int> temp = {nums[j] ,nums[i], nums[left], nums[right]};
-                        res.push_back(temp);
-                        left++;
-                        right--;
-                        
-                        while (left < right && nums[left] == nums[left - 1])
-                            left++;
-                        while (left < right && nums[right] == nums[right + 1])
-                            right--;
-                    } else if (sum < target) {
-                        left++;
-                    } else {
-                        right--;
-                    }
-                }
+                 }
             }
         }
-        return res;
+        return ans;
+         
     }
-
-}
-;
+};
