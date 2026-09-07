dc3ca7e5a9f168b3404befaa95bab52c0914ef43
Sadhana Shrivastava <71784646+sadhana62@users.noreply.github.com>
2026-09-07 22:44:04 +0530

    Time: 13 ms (20.18%), Space: 23.3 MB (58.42%) - LeetHub

---
+++ 1392-longest-happy-prefix/1392-longest-happy-prefix.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string longestPrefix(string s) {
+        int n = s.size();
+        vector<int> lps(n, 0);
+        int len = 0; // length of the previous longest prefix-suffix
+        int i = 1;
+        
+        while (i < n) {
+            if (s[i] == s[len]) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else if (len > 0) {
+                len = lps[len - 1]; // fall back, don't advance i
+            } else {
+                lps[i] = 0;
+                i++;
+            }
+        }
+        
+        return s.substr(0, lps[n - 1]);
+    }
+};
